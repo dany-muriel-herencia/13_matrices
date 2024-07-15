054b6146cdf8a032ff2c8e7dd1e1a5f3e29f32d0
dany-muriel-herencia <drmurielh@unjbg.edu,pe>
2024-07-15 10:44:49 -0500

    acabe el ejercicio 2

---
+++ ejerccicio 2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+/*Halle la suma de los elementos de la diagonal secundaria de una matriz cuadrada.
+*/
+using namespace std;
+int main(){
+         int n,matris[100][100];
+         cout<<"ingrese la matris cuadrada";cin>>n;
+             
+    for(int i=0 ;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"ingrese un numero en la matris: ";
+            cin>>matris[i][j];        }
+    }
+    int suma=0;
+    //imprime la matris
+        for(int k=0 ;k<n;k++){
+            for(int l=0;l<n;l++){
+                 cout<<matris[k][l]<<" ";
+            }
+            cout<<endl;
+
+        }
+    
+    for(int y=0;y<n;y++){
+            suma+=matris[y][n-1-y];
+        }
+    
+    cout<<suma;
+
+    return 0;
+}
